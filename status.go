23ebee881656d634e1e9f44c5839f0d6261e34b9
buchenglei <mybc724@163.com>
2016-01-12 13:17:52 +0800

    修正服务器响应客户端时，不能正确设置状态码的错误

--- status.go
+++ status.go
@@ -6,15 +6,18 @@ import(
 
 // 404 Not Found
 func status404(res Resource) {
+	res.W.WriteHeader(404)
 	fmt.Fprintf(res.W, "<b>404 Not Found</b>")
 }
 
 // 405 Method Not Allowed
 func status405(res Resource) {
+	res.W.WriteHeader(405)
 	fmt.Fprintf(res.W, "<b>405 Method Not Allowed</b>")
 }
 
 // 400 Bad Request
 func status400(res Resource) {
+	res.W.WriteHeader(400)
 	fmt.Fprintf(res.W, "<b>400 Bad Request</b>")
 }
