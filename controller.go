813b096aaa60c352e64fd451444e908f40fa33c5
buchenglei <mybc724@163.com>
2016-01-20 11:05:55 +0800

    controller添加一个解析JSON的函数

--- controller.go
+++ controller.go
@@ -2,6 +2,8 @@ package miniweb
 
 import (
 	"html/template"
+	"encoding/json"
+	"fmt"
 )
 
 type Controller struct {
@@ -12,4 +14,16 @@ type Controller struct {
 func (c Controller) Render(res Resource, temp string, data interface{}) {
 	t, _ := template.ParseFiles(temp)
 	t.Execute(res.W, data)
+}
+
+// 向客户端返回JSON编码格式的数据
+func (c Controller) RenderJSON(res Resource, data interface{}) {
+	res.W.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		res.W.WriteHeader(200)
+		fmt.Fprintf(res.W, string(jsonData))
+	}
 }
