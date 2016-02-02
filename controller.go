a0a0d5ca6f3c3cea164b3716e7b83fce485f285a
buchenglei <mybc724@163.com>
2016-02-02 11:23:40 +0800

    优化缓存的逻辑，使得HasCache方法更加通用

--- controller.go
+++ controller.go
@@ -28,11 +28,15 @@ type Controller struct {
 func (c Controller) Render(res Resource, data interface{}) {
 	//var tempfile string
 	// 判断请求的页面是否已经被缓存
-	if isExist, cacheFile := c.hasCache(res.C, res.M); isExist {
+	if isExist, filePath := c.HasCache(res.C, res.M); isExist {
 		// 缓存存在则直接输出缓存
 		// 将打开的文件内容，复制到响应的对象中去
-		io.Copy(res.W, cacheFile)
-		cacheFile.Close()
+		file, err := os.Open(filePath)
+		if err != nil {
+			panic("\n\n存在缓存页面，但缓存页面打开失败，请检查！！！\n\n")
+		}
+		io.Copy(res.W, file)
+		file.Close()
 		return
 	}
 	fmt.Println("没找到缓存文件")
@@ -86,7 +90,7 @@ func (c Controller) Render(res Resource, data interface{}) {
 	// 调试模式下(debug = true)是不需要创建缓存的
 	if !DEBUG {
 		// 创建缓存页面
-		cachefile := c.createCacheFile(res.C, res.M)
+		cachefile := c.CreateCacheFile(res.C, res.M)
 		cachefile.Write(c.buffer.Bytes())
 		cachefile.Close()
 	}
@@ -107,48 +111,43 @@ func (c Controller) RenderJSON(res Resource, data interface{}) {
 // 判断页面缓存是否存在，如果页面缓存过期则也算不存在
 // cn ControllerName
 // mn MethodName
-func (c Controller) hasCache(cn, mn string) (bool, *os.File) {
+func (c Controller) HasCache(cn, mn string) (bool, string) {
 	// 先判断是否开启了调试模式
 	// 如果开启了调试模式则，不缓存页面
 	if DEBUG {
-		return false, nil
+		return false, ""
 	}
 	// 下面是没有开启调试模式，则缓存存在
 	// 直接打开缓存文件
 	// TODO 该过程有待优化
-	cacheFilePath := "./cache/" + cn + "/" + strings.ToLower(mn) + SUFFIX
-	file, err := os.Open(cacheFilePath)
-	if err == nil {
-		// 当页面缓存存在时，并且没有过期
-		// TODO 这里需要处理缓存页面过期的问题
-		fileinfo, err := file.Stat()
-		if err != nil {
-			fmt.Println(err)
-		}
+	cacheFilePath := "cache/" + cn + "/" + strings.ToLower(mn) + SUFFIX
+	// 读取文件信息
+	fileinfo, err := os.Stat(cacheFilePath)
+	if err == nil || os.IsExist(err) {
+		// 当页面缓存存在时		
 		modtime := fileinfo.ModTime()
 		fmt.Println("modtime", modtime)
 		currtime := time.Now()
 		fmt.Println("currtime", currtime)
 		// 获取缓存文件距今创建多久了
 		diff := currtime.Sub(modtime)
-		fmt.Println("diff", int(diff / time.Second))
-		// 当缓存失效时
+		fmt.Println("缓存已经创建了", int(diff / time.Second), "秒.....")
+		// 当缓的创建时间距今已经超过设定的秒数，则缓存失效
 		if int(diff / time.Second) > Conf.Int("cache", "lifetime") {
-			// 当上一次缓存文件的修改时间距今超过了设置的秒数，则缓存失效
 			// 解析模板时，会重新创建缓存文件
-			return false, nil
+			return false, ""
 		}
-		// 将打开的文件指针返回
-		return true, file
+		// 这里返回缓存有效，并返回缓存页面的路径
+		return true, cacheFilePath
 		
 	}
 	
 	// 缓存文件不存在
-	return false, nil
+	return false, ""
 }
 
 // 创建缓存文件并返回打开的文件指针
-func (c Controller) createCacheFile(cn, mn string) *os.File {
+func (c Controller) CreateCacheFile(cn, mn string) *os.File {
 	// 判断cache目录是否存在
 	_, err := os.Stat("cache/")
 	if os.IsNotExist(err) {
